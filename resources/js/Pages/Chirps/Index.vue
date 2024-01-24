34ff8d1571adc0b0d89a8a6241295ef14e1beef5
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-01-24 18:53:00 +0100

    Page properties part 1

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+    const title = 'Title from Vue'
 </script>
 
 <template>
     <div class="min-h-screen flex justify-center items-center bg-slate-400">
-        <span class="bg-black text-white p-6 rounded-full text-3xl">Chirps Index</span>
+            <span class="bg-black text-white p-6 px-12 rounded-full text-3xl">Chirps Index <br/>
+                <small class="text-gray-400 justify-center items-center flex">{{ title }}</small>
+            </span>
+        
     </div>
 </template>
