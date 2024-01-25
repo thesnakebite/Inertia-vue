c102271332d7eae74f32ec53ef52b650703f0279
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-01-25 16:28:26 +0100

    Create form with Inertia Vue3

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
     import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
     import { Head } from '@inertiajs/vue3'
+    import PrimaryButton from '@/Components/PrimaryButton.vue'
 
     defineProps([
         'title', 
@@ -20,7 +21,16 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">{{ subtitle }}</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <form>
+                            <textarea
+                                placeholder="What's on your mind?"
+                                class="block w-full rounded-md border-gray-700 focus:border-none bg-white dark:bg-gray-800 shadow-sm focus:bg-gray-700 dark:focus:bg-gray-800 active:bg-white dark:active:bg-gray-800 focus:outline-none focus:ring-1 focus:ring-emerald-500 focus:ring-offset-1 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                            >
+                            </textarea>
+                            <PrimaryButton class="mt-2">Chirps</PrimaryButton>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
